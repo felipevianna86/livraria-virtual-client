9e473fe9986c91ff9d923c4ec8178b267ef87435
felipevianna <felipe.vianna86@gmail.com>
2021-03-21 20:48:57 -0300

    #2 - Criando classe pra fazer envio via POST de dados.

---
+++ src/main/java/MainPost.java
@@ -0,0 +1,32 @@
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+public class MainPost {
+
+	public static void main(String[] args) {
+		
+		Livro livro = new Livro(4L, "LIVRO D", "ISBN-5457", "GÊNERO D", 15.49, "Autor João");
+		
+		Response response = ClientBuilder.newClient()
+				.target("http://localhost:8080/livraria-virtual")
+				.path("livro")
+				.request()
+				.post(Entity.xml(livro));
+		
+		if(response.getStatus() == Status.CREATED.getStatusCode()) {
+			ItemBusca item = ClientBuilder.newClient()
+					.target(response.getLocation())
+					.request(MediaType.APPLICATION_XML)
+					.get(ItemBusca.class);	
+			
+			System.out.println(item.getLivro().getTitulo());
+		}else if(response.getStatus() == Status.BAD_REQUEST.getStatusCode()) {
+			System.out.println("Verifique se todos os dados estão corretos");
+		}		
+
+	}
+
+}
